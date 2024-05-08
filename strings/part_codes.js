a11d31e97920d616cb3b606bcd25a0e029f5e609
samrawit <166338789+Tizazu12@users.noreply.github.com>
2024-05-07 22:31:38 -0400

    completed

--- strings/part_codes.js
+++ strings/part_codes.js
@@ -1,17 +1,32 @@
 "use strict";
 
- supplierCode:productNumber-size
-//
-ACME:123-L
-DI:12345-M
-ACE:1-12
+ 
+
+ //ACME:123-L
+//DI:12345-M
+//ACE:1-12
 
 function getSupplier(code){
-let = code.substring(0,)
+let end = code.indexOf(":");
+let supplierCode = code.substring(0,end)
+return supplierCode;
 }
-function getProductNumber(code){
+console.log(getSupplier("ACME:123-L"))
 
+//
+function getProductNumber(code){
+  let productStart = code.indexOf(":");
+  let productEnd = code.indexOf("-");
+let ProductNumber = code.substring(productStart +1, productEnd)
+return ProductNumber;
 }
+
+console.log(getProductNumber("ACME:123-L"))
   
-function getSize(code){}
-  
+function getSize(code){
+  let sizestart = code.indexOf("-");
+  let productSize =code.substring(sizestart +1)
+  return productSize
+}
+console.log(getSize("ACME:123-L"))
+
