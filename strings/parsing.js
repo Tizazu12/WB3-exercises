b40d5af0f569ec91c779632d622e3df2133425bb
samrawit <166338789+Tizazu12@users.noreply.github.com>
2024-05-08 23:55:00 -0400

    made changes

--- strings/parsing.js
+++ strings/parsing.js
@@ -4,26 +4,26 @@ let name = "Brenda Kaye";
 //let first =name.indexOf(" ");
 
 
-  //let first = name.substring(0,5);
-  //let last =name.substring(7);
+//let first = name.substring(0,5);
+//let last =name.substring(7);
 
 //console.log("Name: "+ name)
 //console.log("Frist Name: " + first)
 //console.log("Last Name: "+ last);
 
 
-function parseAndDisplayName(fullName){
+function parseAndDisplayName(fullName) {
   let indexoffullName = fullName.substring("")
- let  fvalue = fullName.substring(indexoffullName, );
-  let lvalue =fullName.substring(indexoffullName, );
-  
- fullName = "frist name:"+ fvalue +  "\n"+"last name:" + lvalue;
-  
- return fullName
+  let fvalue = fullName.substring(indexoffullName,);
+  let lvalue = fullName.substring(indexoffullName,);
+
+  fullName = "frist name:" + fvalue + "\n" + "last name:" + lvalue;
+
+  return fullName
 }
-  
-  let person1 ="Brenda Kaye";
-  let person2="Ian Auston";
-let person3 = "siddalee Grace";   
+
+let person1 = "Brenda Kaye";
+let person2 = "Ian Auston";
+let person3 = "siddalee Grace";
 
 console.log(parseAndDisplayName(person1))
--- strings/part_codes.js
+++ strings/part_codes.js
@@ -1,31 +1,31 @@
 "use strict";
 
- 
 
- //ACME:123-L
+
+//ACME:123-L
 //DI:12345-M
 //ACE:1-12
 
-function getSupplier(code){
-let end = code.indexOf(":");
-let supplierCode = code.substring(0,end)
-return supplierCode;
+function getSupplier(code) {
+  let end = code.indexOf(":");
+  let supplierCode = code.substring(0, end)
+  return supplierCode;
 }
 console.log(getSupplier("ACME:123-L"))
 
 //
-function getProductNumber(code){
+function getProductNumber(code) {
   let productStart = code.indexOf(":");
   let productEnd = code.indexOf("-");
-let ProductNumber = code.substring(productStart +1, productEnd)
-return ProductNumber;
+  let ProductNumber = code.substring(productStart + 1, productEnd)
+  return ProductNumber;
 }
 
 console.log(getProductNumber("ACME:123-L"))
-  
-function getSize(code){
+
+function getSize(code) {
   let sizestart = code.indexOf("-");
-  let productSize =code.substring(sizestart +1)
+  let productSize = code.substring(sizestart + 1)
   return productSize
 }
 console.log(getSize("ACME:123-L"))
